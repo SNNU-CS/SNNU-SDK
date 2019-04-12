06671f5d182eeafbf91a28d54b94614f5034b14d
Qi Zhao <zhaoqi99@outlook.com>
2019-04-12 19:06:48 +0800

    test(test_campus): add test_get_list method

---
+++ test/test_campus.py
@@ -0,0 +1,47 @@
+import unittest
+import urllib.request
+from urllib.error import URLError
+
+from snnusdk.campus import Campus
+
+
+class TestCampus(unittest.TestCase):
+    consumption_status = 200
+    photo_status = 200
+
+    def setUp(self):
+        try:
+            consumption_status = urllib.request.urlopen(
+                url=Campus.URLs.CONSUMPTION, timeout=5).code
+        except URLError:
+            consumption_status = -1
+
+    @unittest.skipIf(consumption_status != 200, "状态码不等于200，就跳过该测试")
+    def test_get_list_1(self):
+        test = Campus('201608735')
+        test_result = test.get_list()
+        self.assertIsInstance(test_result, dict)
+        self.assertTrue(test_result['success'])
+        self.assertEqual(test_result['msg'], '查询成功')
+        self.assertIsInstance(test_result['result'], list)
+
+        test = Campus('xxxxxx')
+        test_result = test.get_list()
+        self.assertIsInstance(test_result, dict)
+        self.assertFalse(test_result['success'])
+        self.assertEqual(test_result['msg'], '请核对校园卡号')
+        self.assertListEqual(test_result['result'], [])
+
+    @unittest.skipIf(consumption_status == 200, "状态码等于200，就跳过该测试")
+    def test_get_list_2(self):
+        test = Campus('201608735')
+        test_result = test.get_list()
+        self.assertIsInstance(test_result, dict)
+        self.assertTrue(test_result['success'])
+        self.assertEqual(test_result['msg'], '网络连接失败')
+        self.assertIsInstance(test_result['result', list])
+        self.assertListEqual(test_result['result'], [])
+
+
+if __name__ == '__main__':
+    unittest.main()
