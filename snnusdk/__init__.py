6ecba93325749e0da67a586a03bf5fc6f77aa109
Qi Zhao <zhaoqi99@outlook.com>
2019-04-13 14:15:23 +0800

    test(test_library): add test_borrow_info method

--- snnusdk/__init__.py
+++ snnusdk/__init__.py
@@ -1,5 +1,5 @@
 from snnusdk.campus import Campus
-from snnusdk.libiary import Library, get_borrow_info
+from snnusdk.libiary import Library
 from snnusdk.message import News, Notice, get_support_dep
 from snnusdk.room import Room
 from snnusdk.urp import Urp
