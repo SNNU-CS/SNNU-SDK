8283d8c722d1ed61e35822fb803bbaff4515759d
Qi Zhao <956361916@qq.com>
2019-04-14 20:13:20 +0800

    ci: fix typos

--- publish-docs.sh
+++ publish-docs.sh
@@ -1,19 +1,13 @@
 #!/bin/bash
 export GH_REF="github.com/snnucs/SNNU-SDK.git"
 cd docs && make html
-pwd
 git clone https://${GH_REF} html
 cd html
 git checkout  gh-pages
 git pull origin
-cd ..
-pwd
-ls
-sudo cd SNNU-SDK-docs && sudo mkdir .git
-sudo mv html/.git/ SNNU-SDK-docs/.git/
-sudo cd SNNU-SDK-docs
-pwd
-rm -rf doctrees
+rm -rf *
+sudo mv  ./../SNNU-SDK-docs/html/* ./
+git status
 git config  --global user.name "Qi Zhao"
 git config  --global user.email "956361916@qq.com" 
 git add . 
